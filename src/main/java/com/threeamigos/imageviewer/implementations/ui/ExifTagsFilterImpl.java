aa70e733621f82b3dea15a2aaf6ea4e564640433
Stefano Reksten <stefano.reksten@gmail.com>
2023-01-14 21:51:54 +0100

    Reworked a bit the exif filters window

--- src/main/java/com/threeamigos/imageviewer/implementations/ui/ExifTagsFilterImpl.java
+++ src/main/java/com/threeamigos/imageviewer/implementations/ui/ExifTagsFilterImpl.java
@@ -1,7 +1,8 @@
 package com.threeamigos.imageviewer.implementations.ui;
 
-import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -13,73 +14,85 @@ import java.util.Map.Entry;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
+import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.ListSelectionModel;
+import javax.swing.WindowConstants;
 
 import com.threeamigos.imageviewer.data.ExifTag;
 import com.threeamigos.imageviewer.interfaces.ui.ExifTagsFilter;
 
 public class ExifTagsFilterImpl implements ExifTagsFilter {
 
+	private static final String OK_OPTION = "OK";
+	private static final String CANCEL_OPTION = "Cancel";
+
+	private boolean selectionSuccesful = false;
+
 	@Override
 	public Map<ExifTag, Collection<String>> filterTags(Map<ExifTag, Collection<String>> map) {
+
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
 		Map<ExifTag, JList<String>> tagsToSelectedValues = new EnumMap<>(ExifTag.class);
 
-		int componentCount = 0;
 		for (Entry<ExifTag, Collection<String>> entry : map.entrySet()) {
 
 			ExifTag tag = entry.getKey();
-			
+
 			List<String> values = new ArrayList<>();
 			values.addAll(entry.getValue());
 			Collections.sort(values);
 
 			JList<String> list = new JList(values.toArray());
-			
 			list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-			list.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 			list.setFixedCellWidth(400);
 
-			JLabel label = new JLabel(tag.getDescription());
-			label.setLabelFor(list);
+			JPanel listPanel = new JPanel();
+			listPanel.setBorder(BorderFactory.createTitledBorder(tag.getDescription()));
+			listPanel.add(list);
 
-			Box labelBox = Box.createHorizontalBox();
-			labelBox.add(label);
-			labelBox.add(Box.createHorizontalGlue());
-			panel.add(labelBox);
-			panel.add(Box.createVerticalStrut(5));
-			Box listBox = Box.createHorizontalBox();
-			listBox.add(list);
-			listBox.add(Box.createHorizontalGlue());
-			panel.add(listBox);
+			panel.add(listPanel);
+
 			panel.add(Box.createVerticalStrut(5));
 
 			tagsToSelectedValues.put(tag, list);
-
-			if (componentCount < map.size() - 1) {
-				JSeparator separator = new JSeparator();
-				panel.add(separator);
-				panel.add(Box.createVerticalStrut(5));
-				componentCount++;
-			}
 		}
 
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		
-		JScrollPane scrollPane = new JScrollPane(panel);
-		
-		int response = JOptionPane.showConfirmDialog(null, scrollPane, "Select tags to filter by", JOptionPane.OK_CANCEL_OPTION);
 
-		if (response == JOptionPane.CANCEL_OPTION) {
+		JScrollPane scrollPane = new JScrollPane(panel);
+
+		String[] options = { OK_OPTION, CANCEL_OPTION };
+
+		JOptionPane optionPane = new JOptionPane(scrollPane, -1, JOptionPane.OK_CANCEL_OPTION, null, options,
+				options[1]);
+
+		JDialog dialog = optionPane.createDialog(null, "Select tags to filter by");
+
+		dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		dialog.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dialog.setVisible(false);
+			}
+		});
+
+		dialog.pack();
+		dialog.setVisible(true);
+
+		if (OK_OPTION.equals(optionPane.getValue())) {
+			selectionSuccesful = true;
+		}
+
+		dialog.dispose();
+
+		if (!selectionSuccesful) {
 			return null;
 		}
 
