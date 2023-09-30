b43a3ba5edf21f574eb3cbb722f71966f9224f92
Stefano Reksten <stefano.reksten@gmail.com>
2023-09-30 15:26:13 +0200

    Comment modified

--- src/main/java/com/threeamigos/pixelpeeper/interfaces/preferences/PreferencesManager.java
+++ src/main/java/com/threeamigos/pixelpeeper/interfaces/preferences/PreferencesManager.java
@@ -3,10 +3,10 @@ package com.threeamigos.pixelpeeper.interfaces.preferences;
 import com.threeamigos.pixelpeeper.interfaces.persister.Persistable;
 
 /**
- * A class that takes care of the whole lifecycle of a set of preferences. The
+ * A class that takes care of the lifecycle of a set of preferences. The
  * persist() method, when called, should result in preferences being saved.
  * 
- * @author stefano
+ * @author Stefano Reksten
  *
  * @param <T>
  */
