28c79fef2b9572c75d8f3f2da3f041e64f026f66
Stefano Reksten <stefano.reksten@gmail.com>
2024-04-02 23:32:18 +0200

    Documentation added

--- src/main/java/com/threeamigos/pixelpeeper/interfaces/datamodel/ExifReader.java
+++ src/main/java/com/threeamigos/pixelpeeper/interfaces/datamodel/ExifReader.java
@@ -8,7 +8,9 @@ import java.util.Optional;
 /**
  * An interface able to retrieve {@link com.threeamigos.pixelpeeper.data.ExifTag}s from
  * image files. There is no guarantee we can retrieve EXIF tags as the image could have been
- * post-processed and saved without the EXIF tags.
+ * post-processed and saved without the EXIF tags.<br/>
+ * This interface will retrieve the EXIF tags <b>only</b>, and will <b>not</b> load the whole image.
+ * For that, see {@link ExifImageReader}.
  *
  * @author Stefano Reksten
  */
--- src/main/java/com/threeamigos/pixelpeeper/interfaces/datamodel/NamePattern.java
+++ src/main/java/com/threeamigos/pixelpeeper/interfaces/datamodel/NamePattern.java
@@ -2,10 +2,26 @@ package com.threeamigos.pixelpeeper.interfaces.datamodel;
 
 import java.io.File;
 
+/**
+ * An interface used to massively rename files, using one or more EXIF tags in order to
+ * compose the new file name. According to a name pattern, it will substitute parts contained
+ * within the left and right delimiter with the actual value of EXIF tags found within the image
+ * itself. E.g. if specifying {FILENAME}_shot_with_{CAMERA_MODEL} it could for example produce
+ * IMG0000_shot_with_Panasonic GX9.jpg.
+ * See also {@link FileRenamer}.
+ *
+ * @author Stefano Reksten
+ */
 public interface NamePattern {
 
+    /**
+     * Left delimiter
+     */
     String LEFT_BRACKET = "{";
 
+    /**
+     * Right delimiter
+     */
     String RIGHT_BRACKET = "}";
 
     boolean rename(File file);
