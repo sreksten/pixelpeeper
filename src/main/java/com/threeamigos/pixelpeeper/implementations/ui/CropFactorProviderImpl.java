c68ada90f336df5a4e3bafd0dd488c96e150a2c2
Stefano Reksten <stefano.reksten@gmail.com>
2024-04-05 01:12:14 +0200

    Exception handled

--- src/main/java/com/threeamigos/pixelpeeper/implementations/ui/CropFactorProviderImpl.java
+++ src/main/java/com/threeamigos/pixelpeeper/implementations/ui/CropFactorProviderImpl.java
@@ -36,6 +36,7 @@ public class CropFactorProviderImpl implements CropFactorProvider {
                 try {
                     Thread.sleep(200);
                 } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
             }
         }
