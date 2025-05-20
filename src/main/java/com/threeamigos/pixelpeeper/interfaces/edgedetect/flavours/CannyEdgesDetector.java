654ed5e3e15361ebb98e94885c55879d78dee475
Stefano Reksten <stefano.reksten@gmail.com>
2025-05-21 01:51:00 +0200

    Removed unused methods

--- src/main/java/com/threeamigos/pixelpeeper/interfaces/edgedetect/flavours/CannyEdgesDetector.java
+++ src/main/java/com/threeamigos/pixelpeeper/interfaces/edgedetect/flavours/CannyEdgesDetector.java
@@ -3,30 +3,9 @@ package com.threeamigos.pixelpeeper.interfaces.edgedetect.flavours;
 import com.threeamigos.pixelpeeper.interfaces.edgedetect.EdgesDetector;
 
 /**
- * An interface exposing all parameters the Canny edge-detection algorithm accepts.
+ * An interface designed to implement the Canny Edges Detector algorithm.
  *
  * @author Stefano Reksten
  */
 public interface CannyEdgesDetector extends EdgesDetector {
-
-    float getLowThreshold();
-
-    void setLowThreshold(float lowThreshold);
-
-    float getHighThreshold();
-
-    void setHighThreshold(float highThreshold);
-
-    float getGaussianKernelRadius();
-
-    void setGaussianKernelRadius(float gaussianKernelRadius);
-
-    int getGaussianKernelWidth();
-
-    void setGaussianKernelWidth(int gaussianKernelWidth);
-
-    boolean isContrastNormalized();
-
-    void setContrastNormalized(boolean contrastNormalized);
-
 }
